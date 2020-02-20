367352e0b55c8f989d77fa4882693cdc00dc9143
Basem <basem.jawahri@centrica.com>
2020-02-20 16:19:30 +0000

    chore: config linting

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,14 @@
+/* eslint-env node */
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    'self-closing-void-elements': false,
+    'no-triple-curlies': false,
+    'style-concatenation': false
+  },
+  ignore: [
+    './node_modules/**'
+  ]
 };
