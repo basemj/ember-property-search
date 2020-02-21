9fd00a1d7f160cb15d0456ff18556f948b1be989
Basem <3786429+basemj@users.noreply.github.com>
2020-02-21 03:47:10 +0000

    refactor: styling and user experience

--- app/components/search/property-types/component.js
+++ app/components/search/property-types/component.js
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 import {get, computed} from '@ember/object';
 
 export default Component.extend({
+  propertyTypes: [],
   typeSelected: computed.filter('propertyTypes.@each.checked', function (type) {
     return type.checked === true;
   }),
@@ -9,7 +10,8 @@ export default Component.extend({
   actions: {
     onChangeTypeSelection(propertyType) {
       const onChangeTypeSelection = get(this, 'onChangeTypeSelection');
-      if (onChangeTypeSelection) {
+      const typeSelectedId = get(this, 'typeSelected.firstObject.id');
+      if (onChangeTypeSelection && propertyType.id !== typeSelectedId) {
         onChangeTypeSelection(propertyType);
       }
     }
